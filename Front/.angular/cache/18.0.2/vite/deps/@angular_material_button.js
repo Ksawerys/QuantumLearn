16085781fbe79cebce4dd115987ad30cd54cae99
Javier Velasco Alcalde <145055220+Ksawerys@users.noreply.github.com>
2024-06-03 23:32:06 +0200

    librerias

---
+++ Front/.angular/cache/18.0.2/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-NTMI6TDV.js";
+import "./chunk-WQN44F6X.js";
+import "./chunk-ZBN42LM6.js";
+import "./chunk-ZXXU3F62.js";
+import "./chunk-Y4SCNN5M.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
